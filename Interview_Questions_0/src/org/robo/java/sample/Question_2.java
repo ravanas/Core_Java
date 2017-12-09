6ae1b9368414f9748d2b87e35cacc455597d9126
Robaguar <34389458+Robaguar@users.noreply.github.com>
2017-12-09 11:39:14 +0530

    Changes

--- Interview_Questions_0/src/org/robo/java/sample/Question_2.java
+++ Interview_Questions_0/src/org/robo/java/sample/Question_2.java
@@ -2,6 +2,13 @@ package org.robo.java.sample;
 
 import org.robo.java.Question_1_MainMethod;
 
+//Question 5: Is it possible to create 2 classes in same name ?
+//Yes... Then how to call them 
+//By using the full name i.e path 
+//Here is the example Question_1_MainMethod is the class declared in 2 different package it is differentiate only by using full path
+//Question_1_MainMethod --------------------------inside the same package
+//org.robo.java.Question_1_MainMethod-------------from different package
+
 public class Question_2 {
 	public Question_2() {
 		Question_1_MainMethod qm = new Question_1_MainMethod();
